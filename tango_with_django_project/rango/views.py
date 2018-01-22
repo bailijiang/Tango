6883ed2aea1d8865e1605544f508f9ee4c9e1ad0
bailijiang <bailijiang@hotmail.com>
2018-01-22 17:43:45 +0800

    Tango 6 finish..

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 
 def index(request):
-    context_dict = {'boldmessages': "Crunchy, creamy, cookie, candy, cupcake"}
-
+    # context_dict = {'boldmessages': "Crunchy, creamy, cookie, candy, cupcake"}
     # return HttpResponse("Rango says hey there partner!</br><a href='/rango/about'>about</a>")
-    return render(request, 'rango/index.html', context=context_dict)
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 
     # return HttpResponse("Rango says here is the about page. </br><a href='/rango/'>Index</a>")
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
